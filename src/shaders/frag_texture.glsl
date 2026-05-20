40f73a4991b8f909b153d2a86fadb570b544b47e
wolftender <etheryt@protonmail.com>
2026-05-20 16:19:30 +0200

    flip textures in the texture view to be displayed correctly

--- src/shaders/frag_texture.glsl
+++ src/shaders/frag_texture.glsl
@@ -9,5 +9,6 @@ uniform sampler2D u_texture;
 out vec4 frag_color;
 
 void main() {
-    frag_color = vec4(texture(u_texture, fs_in.uv).rgba);
+    vec2 uv = vec2(fs_in.uv.x, 1.0 - fs_in.uv.y);
+    frag_color = vec4(texture(u_texture, uv).rgba);
 }
